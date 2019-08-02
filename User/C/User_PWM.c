6715ecd9e5ef87b2a28dcf9ccb6eab8f41784077
techguy10k <47449920+techguy10k@users.noreply.github.com>
2019-08-02 16:19:44 +0800

    pwm输入范围与中心位置改变

--- User/C/User_PWM.c
+++ User/C/User_PWM.c
@@ -4,15 +4,35 @@
 /* 设置通道一的位置 */
 void ChannelOne_SetPositon(float Position)
 {
-	float temp = 500;
-	temp += 2000.0 * Position;
-	__HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_1,(uint16_t)temp);
+	float temp = 1500;
+	
+	if(Position > 5000)
+	{
+		Position = 5000;
+	}
+
+	if(Position < -5000)
+	{
+		Position = -5000;
+	}	
+	
+	temp += Position *0.2;
 }
 /* 设置通道二的位置 */
 void ChannelTwo_SetPositon(float Position)
 {
-	float temp = 500;
-	temp += 2000.0 * Position;
-	__HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_2,(uint16_t)temp);	
+	float temp = 1500;
+	
+	if(Position > 5000)
+	{
+		Position = 5000;
+	}
+
+	if(Position < -5000)
+	{
+		Position = -5000;
+	}	
+	
+	temp += Position *0.2;	
 }
 
